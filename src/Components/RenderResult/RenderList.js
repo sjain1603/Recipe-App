be604011f6528ce918407947965dbfa24b9a4de6
sjain1603 <sjain1@ec.iitr.ac.in>
2020-07-02 14:30:30 +0530

    Added Labels

--- src/Components/RenderResult/RenderList.js
+++ src/Components/RenderResult/RenderList.js
@@ -1,12 +1,81 @@
 import React, { Component } from 'react';
-import { Table } from 'reactstrap';
+import { Table, Input } from 'reactstrap';
 import Tabs from '../Tabs/Tabs';
 import './Styles.scss';
 
 class RenderList extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            highProtien: false,
+            highCarb: false,
+            regular: false,
+            balanced: false,
+        }
         this.renderFood = this.renderFood.bind(this);
+    }
+
+    computeFoodLabel = ( nutrients ) => {
+        var ratio = nutrients.PROCNT / (nutrients.PROCNT + nutrients.CHOCDF + nutrients.FAT)
+        var label1 = 'Regular';
+        var label2 = '';
+        if(ratio > 0.15) label1 = 'High Protien';
+        if (ratio < 0.5) label2 = 'High Carb';
+        if(ratio <= 0.15 && ratio >= 0.12){
+            label1 = 'Balanced';
+        }
+
+        if(label1 && label2) return (
+            <>
+            {label1}
+            <br />
+            {label2}
+            </>
+        );
+        else return (
+            <>
+            {label1}
+            </>
+        );
+    }
+
+    computeRecipeLabel = (nutrients) => {
+        var ratio = nutrients.PROCNT.quantity / (nutrients.PROCNT.quantity + nutrients.CHOCDF.quantity + nutrients.FAT.quantity)
+        var label1 = 'Regular';
+        var label2 = '';
+        if(ratio > 0.15) label1 = 'High Protien';
+        if (ratio < 0.5) label2 = 'High Carb';
+        if(ratio <= 0.15 && ratio >= 0.12){
+            label1 = 'Balanced';
+        }
+
+        if(label1 && label2) return (
+            <>
+            {label1}
+            <br />
+            {label2}
+            </>
+        );
+        else return (
+            <>
+            {label1}
+            </>
+        );
+    }
+
+    calcRatio = (nutrients) => {
+        return nutrients.PROCNT.quantity / (nutrients.PROCNT.quantity + nutrients.CHOCDF.quantity + nutrients.FAT.quantity)
+    }
+
+    filterRecipe = () => {
+        const {recipeResults} = this.state;
+        
+        if(this.state.highProtien){
+            recipeResults.filter(recipe => {
+                return this.calcRatio(recipe.recipe.totalNutrients) > 0.15
+            })
+        }
+
     }
 
     renderFood = () => { 
@@ -19,8 +88,9 @@ class RenderList extends Component{
                         <thead>
                             <tr>
                                 <th>Image</th>
-                                <th>Label</th>
+                                <th>Food</th>
                                 <th>Category</th>
+                                <th>Diet Label</th>
                                 <th>Energy (kcal)</th>
                             </tr>
                         </thead>
@@ -34,6 +104,7 @@ class RenderList extends Component{
                                         </td>
                                         <td>{dish.food.label}</td>
                                         <td>{dish.food.category}</td>
+                                        <td>{this.computeFoodLabel(dish.food.nutrients)}</td>
                                         <td>{dish.food.nutrients.ENERC_KCAL}</td>
                                     </tr> 
                                 );
@@ -71,7 +142,7 @@ class RenderList extends Component{
                                                 alt={recipe.recipe.label} />
                                         </td>
                                         <td>{recipe.recipe.label}</td>
-                                        <td>{recipe.recipe.dietLabels[0]}</td>
+                                        <td>{this.computeRecipeLabel(recipe.recipe.totalNutrients)}</td>
                                         <td>{recipe.recipe.yield}</td>
                                         <td>{recipe.recipe.calories}</td>
                                     </tr> 
@@ -91,6 +162,26 @@ class RenderList extends Component{
                     {this.renderFood()}
                 </div>
                 <div label="Recipe">
+                    <div className="filter">
+                        <b>Diet Label Filter:</b>
+                        <label>
+                            <Input type='checkbox' id="High Protien" name="High Protien" />
+                            High Protien
+                        </label>
+                        <label>
+                            <Input type='checkbox' id="High Protien" name="High Protien" />
+                            High Carb
+                        </label>
+                        <label>
+                            <Input type='checkbox' id="High Protien" name="High Protien" />
+                            Regular
+                        </label>
+                        <label>
+                            <Input type='checkbox' id="High Protien" name="High Protien" />
+                            Balanced
+                        </label>
+                    </div>
+                    <br />
                     {this.renderRecipe()}
                 </div>
             </Tabs>
